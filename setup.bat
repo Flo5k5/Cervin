b8782a6e11a7a3bbb4d9d333f121390d2522bd87
Florian <florian.nourisse@mail.sogeti.com>
2013-04-10 13:19:40 +0200

    Modification setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,6 @@
+@echo off
+if exist "%~dp0\vendor\.composer\files\" del /f /q /s "%~dp0\vendor\.composer\files\*" && FOR /D %%p IN ("%~dp0\vendor\.composer\files\*") DO rmdir "%%p" /s /q
+if exist "%LOCALAPPDATA%\Composer\files\" del /f /q /s "%LOCALAPPDATA%\Composer\files\*" && FOR /D %%p IN ("%LOCALAPPDATA%\Composer\files\*") DO rmdir "%%p" /s /q
 php composer.phar self-update
 php composer.phar update
+pause > nul
