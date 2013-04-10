594be17f4e3d805f7548e5e880890f792f526127
Florian <florian.nourisse@mail.sogeti.com>
2013-04-10 13:49:38 +0200

    Modification de commit.bat

--- commit.bat
+++ commit.bat
@@ -2,8 +2,19 @@
 
 set /p msg=Enter commit description :
 
+echo.
+echo git add
+echo.
 git add .
+
+echo.
+echo git commit
+echo.
 git commit -m "%msg%"
+
+echo.
+echo git push
+echo.
 git push origin master
 
 color 0a
