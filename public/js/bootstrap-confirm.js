a94830c0a12a4a83b80ca4063dd4ef3300d2930c
Thibaud Avenier <tavenier@FR-69-01-11-047.corp.capgemini.com>
2013-04-17 09:15:43 +0200

    maj bootstrap-confirm.js

--- public/js/bootstrap-confirm.js
+++ public/js/bootstrap-confirm.js
@@ -5,8 +5,8 @@
             var html = '<div class="modal" id="confirmContainer"><div class="modal-header"><a class="close" data-dismiss="modal">×</a>' +
             '<h3>#Heading#</h3></div><div class="modal-body">' +
             '#Body#</div><div class="modal-footer">' +
-            '<a href="#" class="btn btn-primary" id="confirmYesBtn">Confirm</a>' +
-            '<a href="#" class="btn" data-dismiss="modal">Close</a></div></div>';
+            '<a href="#" class="btn btn-primary" id="confirmYesBtn">Confirmer</a>' +
+            '<a href="#" class="btn" data-dismiss="modal">Fermer</a></div></div>';
 
             var defaults = {
                 heading: 'Please confirm',
@@ -18,7 +18,7 @@
             html = html.replace('#Heading#',options.heading).replace('#Body#',options.body);
             $(this).html(html);
             $(this).modal('show');
-            var context = $(this); 
+            var context = $(this);
             $('#confirmYesBtn',this).click(function(){
                 if(options.callback!=null)
                     options.callback();
