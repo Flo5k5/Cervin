4aa78655d805eec320cc329228b935bbb4c50b90
Florian <florian.nourisse@mail.sogeti.com>
2013-04-18 10:37:19 +0200

    Ajout de la librairie pour les tests

--- module/Core/src/Collection/Test/Entity/ChampTest.php
+++ module/Core/src/Collection/Test/Entity/ChampTest.php
@@ -1,48 +1,18 @@
 <?php
 namespace Collection;
 
-use Doctrine\DBAL\Schema\SchemaException;
-use Doctrine\ORM\Tools\SchemaTool;
+use TestsCervin\Doctrine;
 
-// $this->application instance of Zend\Mvc\Application
-// $this->sm instance of Zend\ServiceManager\ServiceManager
-// $this->em instance of Doctrine\ORM\EntityManager
-
-class ChampTest extends \PHPUnit_Framework_TestCase
+class ChampTest extends Doctrine
 {
-	protected $application;
-    protected $sm;
-	protected $em;
-    protected $bootstrap;
-	protected $emAlias;
-	
     public function setUp()
     {
-		$this
-            ->setBootstrap(__DIR__ . '/../../../../../../tests/bootstrap.php')
-            ->setEmAlias('doctrine.entitymanager.orm_default');
-			
-        $application = require $this->bootstrap;
-        $this->application = $application;
-        $this->sm = $application->getServiceManager();
-        $this->em = $this->sm->get($this->emAlias);
-
-        $metadatas = $this->em->getMetadataFactory()->getAllMetadata();
-        if (!empty($metadatas)) {
-            $tool = new SchemaTool($this->em);
-            //$tool->createSchema($metadatas);
-        } else {
-            throw new SchemaException(
-                'No metadata classes to process'
-            );
-        }
+        parent::setUp();
     }
 
     public function tearDown()
     {
-        unset($this->application);
-        unset($this->sm);
-        unset($this->em);
+        parent::tearDown();
     }
 
     public function testChampInitialState()
@@ -55,47 +25,22 @@ class ChampTest extends \PHPUnit_Framework_TestCase
         $this->assertNull($champ->datas, '"datas" should initially be null');
         $this->assertNull($champ->type_element, '"type_element" should initially be null');
     }
-    
-    public function testChampSaveEntity(){ 	
-		$nom = 'Nom';
-    	$type = 'artefact';
-    	$typeElement = new Entity\TypeElement($nom, $type);
 
-		$label = 'Label';
-		$description = 'Description';
-		$format = 'texte';
-		
-    	$champ = new Entity\Champ($label, $typeElement, $format);
-    	$champ->description = $description;
-    	
-    	$this->em->persist($typeElement);
-    	$this->em->persist($champ);
-    	$this->em->flush();
-    }
+    public function testChampSaveEntity()
+    {
+        $nom = 'Nom';
+        $type = 'artefact';
+        $typeElement = new Entity\TypeElement($nom, $type);
 
-    protected function setBootstrap($bootstrap)
-    {
-        $this->bootstrap = $bootstrap;
-        return $this;
-    }
-    
-    protected function setEmAlias($emAlias)
-    {
-        $this->emAlias = $emAlias;
-        return $this;
-    }
+        $label = 'Label';
+        $description = 'Description';
+        $format = 'texte';
 
-    public function testServiceManagerInstance()
-    {
-        $this->assertInstanceOf(
-            'Zend\ServiceManager\ServiceManager',
-            $this->sm);
-    }
-	
-    public function testEmInstance()
-    {
-        $this->assertInstanceOf(
-            'Doctrine\ORM\EntityManager',
-            $this->em);
+        $champ = new Entity\Champ($label, $typeElement, $format);
+        $champ->description = $description;
+
+        $this->em->persist($typeElement);
+        $this->em->persist($champ);
+        $this->em->flush();
     }
 }
