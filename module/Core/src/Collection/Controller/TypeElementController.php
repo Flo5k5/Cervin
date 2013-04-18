c193977c90307a3a2e5fd3bf0b8f7daa3cab7865
Thibaud Avenier <tavenier@FR-69-01-11-047.corp.capgemini.com>
2013-04-18 09:41:05 +0200

    ajout du controller typeElementController

--- module/Core/src/Collection/Controller/TypeElementController.php
+++ module/Core/src/Collection/Controller/TypeElementController.php
@@ -12,12 +12,54 @@ namespace Collection\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Zend\Form\Annotation\AnnotationBuilder;
+use Doctrine\ORM\EntityManager;
 
 class TypeElementController extends AbstractActionController
 {
+
+	/**
+	* @var Doctrine\ORM\EntityManager
+	*/
+    protected $em;
+
+    public function setEntityManager(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+ 
+    /**
+	* Return a EntityManager
+	*
+	* @return Doctrine\ORM\EntityManager
+	*/
+    public function getEntityManager()
+    {
+        if ($this->em === null) {
+            $this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        }
+        
+        return $this->em;
+    }
+
+
+
     public function indexAction()
     {
+
+
+    	$artefact = $this->getEntityManager()->findBy('Collection\Entity\TypeElement', array('type'=>'artefact'));
+
+    	$media = $this->getEntityManager()->findBy('Collection\Entity\TypeElement', array('type'=>'media'));
+
+                
 		
+
+
+    	return array{
+    		'artefact' => $artefact,
+    		'media' => $media,
+    	}
+
     }
 
     public function addAction()
