2a09a80ddc457594fc0943883d0e5d7ab9e0d7f4
Gregoire <gregoire.laillet-de-montulle@sogeti.com>
2013-04-19 15:16:13 +0200

    Maj inputfilter Champ

--- module/Core/src/Collection/Entity/Champ.php
+++ module/Core/src/Collection/Entity/Champ.php
@@ -159,14 +159,6 @@ class Champ implements InputFilterAwareInterface
     				array('name' => 'StripTags'),
     				array('name' => 'StringTrim'),
     			),
-    			'validators' => array(
-    				array(
-    					'name' => 'StringLength',
-    					'options' => array(
-    						'encoding' => 'UTF-8'
-    					),
-    				),
-    			),
     		)));
     	
     		$this->inputFilter = $inputFilter;
