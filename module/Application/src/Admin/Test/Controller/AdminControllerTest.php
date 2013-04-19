1185db51db014cd8d4ab8884d00891427ac2102b
Florian <florian.nourisse@mail.sogeti.com>
2013-04-19 11:48:39 +0200

    Modifications AdminControllerTest

--- module/Application/src/Admin/Test/Controller/AdminControllerTest.php
+++ module/Application/src/Admin/Test/Controller/AdminControllerTest.php
@@ -2,50 +2,90 @@
 
 namespace AdminTest\Controller;
 
+use Zend;
 use Zend\Http\Request;
 use Zend\Http\Response;
-use SamUser\Entity\User;
+use Zend\Mvc\Controller;
+use Zend\Mvc\MvcEvent;
+//use SamUser\Entity\User;
+//use SamUser\Entity\Role;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Admin\Controller\AdminController;
+use Doctrine\ORM\EntityManager;
+use ZfcUser\Entity\UserInterface;
+use ZfcUser\Entity\User;
+use ZfcUser\Controller\Plugin\ZfcUserAuthentication;
+
+
 
 class AdminControllerTest extends AbstractHttpControllerTestCase
 {
 	protected $traceError = true;
 
-
 	protected $request;
+	protected $bootstrap;
 
     public function setUp()
     {
-        $this->setApplicationConfig(
+        /*$this->setApplicationConfig(
             include __DIR__ .'/../../../../../../config/application.config.php'
-        );
-
+        );*/
+		
+    	/*$di = new Di();
+    	$di->instanceManager()->addTypePreference('Zend\ServiceManager\ServiceLocatorInterface', 'Zend\ServiceManager\ServiceManager');
+    	$di->instanceManager()->addTypePreference('Zend\EventManager\EventManagerInterface', 'Zend\EventManager\EventManager');
+    	$di->instanceManager()->addTypePreference('Zend\EventManager\SharedEventManagerInterface', 'Zend\EventManager\SharedEventManager');
+    	$di->instanceManager()->addTypePreference('Zend\EventManager\SharedListenerAggregateInterface', 'Zend\EventManager\SharedListenerAggregate');
+    	*/
+        $this->setBootstrap(__DIR__ . '/../../../../../../tests/bootstrap.php');
+        
+        $application = require $this->bootstrap;
+        $serviceManager = $application->getServiceManager();
+        
+        //$serviceManager = Bootstrap::getServiceManager();
+        //$this->controller = new RegisterController();
+        //$this->controller = $di->newInstance('Admin\Controller\AdminController');
+        $this->controller = new AdminController();
         $this->request    = new Request();
+       // $this->routeMatch = new RouteMatch(array('controller' => 'add'));
+        $this->event      = new MvcEvent();
+        //$config = $serviceManager->get('Config');
+        //$routerConfig = isset($config['router']) ? $config['router'] : array();
+        //$router = HttpRouter::factory($routerConfig);
+        //$this->event->setRouter($router);
+        //$this->event->setRouteMatch($this->routeMatch);
+        $this->controller->setEvent($this->event);
+        $this->controller->setServiceLocator($serviceManager);
+        $mockAuth = $this->getMock('ZfcUser\Entity\UserInterface');
+        
+        $ZfcUserMock = $this -> getMock('ZfcUser\Entity\User');
+        
+        $ZfcUserMock    -> expects($this->any())
+        -> method('getId')
+        -> will($this->returnValue('1'));
+        
+        $authMock = $this->getMock('ZfcUser\Controller\Plugin\ZfcUserAuthentication');
+        
+        $authMock   -> expects($this->any())
+        -> method('hasIdentity')
+        -> will($this->returnValue(true));
+
+        $authMock   -> expects($this->any())
+        -> method('zfcUserIdentity')
+        -> will($this->returnValue(true));
+        
+        $authMock   -> expects($this->any())
+        -> method('getIdentity')
+        -> will($this->returnValue($ZfcUserMock));
+        
+        $this -> controller->getPluginManager()
+        ->setService('zfcUserAuthentication', $authMock);
+
         parent::setUp();
     }
 	
-	/*public function setUp()
-	{
-		$di = Bootstrap::getDi();
-
-		$this->controller = $di->newInstance('Album\Controller\AlbumController');
-
-		$this->request    = new Request();
-		$this->routeMatch = new RouteMatch(array('controller' => 'album'));
-		$this->event      = new MvcEvent();
-		$this->event->setRouteMatch($this->routeMatch);
-		$this->controller->setEvent($this->event);
-
-		$this->controller->setServiceLocator(Bootstrap::getServiceManager());
-	}*/
-
-
-
-
-	
 	public function testEditusersActionCanBeAccessed()
 	{
-
 		$postData = array(
 							'identity' => 'adminlogin',
 	  						'credential' => 'toto123'
@@ -58,18 +98,6 @@ class AdminControllerTest extends AbstractHttpControllerTestCase
 		
 		//$this->dispatch('/user/login');
 		//$this->assertNotXpathQueryContentContains('/html/body/div[2]/div/form/fieldset/ul/li', 'Authentication failed. Please try again.');
-		
-	   // $csrf = $this->_getLoginFormCSRF();
-	 //   $this->resetResponse();
-		
-
-	   // $this->request->setMethod('POST');
-	   // $this->request->setPost(array(
-	   //     'identity' => 'toto',
-	   //     'credential' => 'toto123',
-	   // ));
-
-
 
 		/*$this->dispatch('/admin/gestion-users');
 		$this->assertResponseStatusCode(200);
@@ -81,15 +109,17 @@ class AdminControllerTest extends AbstractHttpControllerTestCase
 		$this->assertControllerClass('AdminController');
 		$this->assertMatchedRouteName('editusers');*/
 	}
+	
+	public function testZfcUserIdentity(){
 
-
-
-/*
-	public function testSuccess()
-    {
-        $this->assertContains(4, array(1, 2, 3, 4));
-    }
-*/
-
-
+		
+		$this->assertTrue($this->zfcUserAuthentication()->hasIdentity());
+	}
+	
+	protected function setBootstrap($bootstrap)
+	{
+		$this->bootstrap = $bootstrap;
+	
+		return $this;
+	}
 }
