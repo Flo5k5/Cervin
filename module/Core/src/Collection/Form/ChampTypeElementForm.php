db2752a15409175fa8dd51e87fc0c742bce7a682
Gregoire <gregoire.laillet-de-montulle@sogeti.com>
2013-04-19 16:15:53 +0200

    Maj formulaires typeElement et ChampTypeElement

---
+++ module/Core/src/Collection/Form/ChampTypeElementForm.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Collection\Form;
+
+use Zend\Form\Form;
+use InvalidArgumentException;
+use Collection\Entity\TypeElement;
+
+class ChampTypeElementForm extends Form
+{
+	public function __construct($type_element, $name = null)
+	{
+		if (!$type_element instanceof TypeElement) {
+			throw new InvalidArgumentException('Construction d\'un formulaire TypeElementForm avec un param�tre qui n\'est pas de type TypeElement');
+		}
+		parent::__construct('champtypeelement');
+		$this->setAttribute('method', 'post');
+
+		$this->add(array(
+				'name' => 'id',
+				'attributes' => array(
+						'type'  => 'hidden',
+				),
+		));
+		
+		foreach ($type_element->__get('champs') as $champ) {
+			
+			switch ($champ->__get('format')) {
+				case 'texte':
+					$this->add(array(
+						'name' => $champ->__get('label'),
+						'attributes' => array('type'  => 'text'),
+						'options' => array('label' => $champ->__get('label'))
+					));
+					break;
+				case 'date':
+					$this->add(array(
+						'name' => $champ->__get('label'),
+						'attributes' => array('type'  => 'date'),
+						'options' => array('label' => $champ->__get('label'))
+					));
+					break;
+				case 'nombre':
+					$this->add(array(
+						'name' => $champ->__get('label'),
+						'attributes' => array('type'  => 'number'),
+						'options' => array('label' => $champ->__get('label'))
+					));
+					break;
+				case 'fichier':
+					$this->add(array(
+						'name' => $champ->__get('label'),
+						'attributes' => array('type'  => 'file'),
+						'options' => array('label' => $champ->__get('label'))
+					));
+					break;
+				case 'url':
+					$this->add(array(
+						'name' => $champ->__get('label'),
+						'attributes' => array('type'  => 'url'),
+						'options' => array('label' => $champ->__get('label'))
+					));
+			} // end switch
+			
+		} // end foreach
+
+		
+		$this->add(array(
+				'name' => 'submit',
+				'attributes' => array(
+					'type'  => 'submit',
+					'value' => 'Go',
+					'id' => 'submitbutton'
+				),
+		));
+		
+	}
+	
+}
