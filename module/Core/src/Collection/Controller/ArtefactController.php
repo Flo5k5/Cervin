e17310572ac5afd18c77f8d8b8dcd57b9b296105
Corentin <corentin.gaillot@mail.sogeti.com>
2013-04-22 09:09:02 +0200

    je sais plus ce que j'avais commit la semaine derniere alors je mets tout ca pour avoir une version stable sur le git

--- module/Core/src/Collection/Controller/ArtefactController.php
+++ module/Core/src/Collection/Controller/ArtefactController.php
@@ -7,6 +7,7 @@ namespace Collection\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Collection\Form\TypeElementForm;
 
 class ArtefactController extends AbstractActionController
 {
