110154c199bd82c347c6ba1d52a964dbaac8dc0a
Corentin <corentin.gaillot@mail.sogeti.com>
2013-04-25 10:46:28 +0200

    media creation ok

--- module/Core/src/Collection/Controller/ArtefactController.php
+++ module/Core/src/Collection/Controller/ArtefactController.php
@@ -51,74 +51,8 @@ class ArtefactController extends AbstractActionController
 
     public function ajouterAction()
     {
-<<<<<<< HEAD
-    	$request = $this->getRequest();
-    	if ($request->isPost()) {
-    		// Le formulaire d'ajout a été posté
-    		// On récupère le type de l'artefact que l'on va créer
-	    	$nom_type_element = $this->params()->fromRoute('type_element');
-	    	$type_element = $this->getEntityManager()->getRepository('Collection\Entity\TypeElement')->findOneBy(array('type'=>'artefact', 'nom'=>$nom_type_element));
-	    	if (!$type_element) {
-	    		throw new Exception('Type d\'élément non trouvé au moment de la création de l\'artefact');
-	    	}
-	    	$form = new ChampTypeElementForm($type_element);
-	    	$form->setInputFilter($album->getInputFilter());
-	    	$form->setData($request->getPost());
-	    	$artefact = new Artefact(null, $type_element);
-	    	if ($form->isValid()) {
-					$upload = new Http();
-					$upload->setDestination('upload');
-				    $upload->receive();
-	    		$datas = $form->getData();
-	    		// On initialise les champs id, titre et description de l'artefact
-	    		$artefact->populate($datas);
-	    		// Reste tous les autres : il faut créer des entity 'data'
-	    		foreach ($datas as $label => $data) {
-	    			// on passe les champs inutils
-	    			if ($label != 'id' && $label != 'titre' && $label != 'description' && $label != 'submit') {
-	    				// On vérifie que ce type d'artefact a bien un champ qui porte ce nom
-	    				$champ = $this->getEntityManager()->getRepository('Collection\Entity\Champ')->findOneBy(array('label'=>$label, 'type_element'=>$type_element));
-	    	 			if (!$champ) {
-	    	 				throw new Exception('Champ '.$label.' non trouvé.');
-	    	 			}
-	    	 			// On créer l'entity data qui correspond à ce champ pour cet artefact
-	    	 			$databd = new Data($artefact, $champ);
-	    	 			switch ($champ->format) {
-	    	 				case 'texte':
-	    	 					$databd->texte = $data;    	 					
-	    	 					break;
-	    	 				case 'textarea':
-	    	 					$databd->textarea = $data;
-	    	 					break;
-	    	 				case 'date':
-	    	 					$databd->date = $data;
-	    	 					break;
-	    	 				case 'nombre':
-	    	 					$databd->nombre = $data;
-	    	 					break;
-	    	 				case 'fichier':
-	    	 					$databd->fichier = $data;
-	    	 					break;
-	    	 				case 'url':
-	    	 					$databd->url = $data;
-	    	 			} // end switch
-	    	 			$this->getEntityManager()->persist($databd);
-	    			}
-	    		}
-	    		$this->getEntityManager()->persist($artefact);
-	    		$this->getEntityManager()->flush();
-	    		return $this->redirect()->toRoute('collection/consulter');
-	    	} else {
-
-	    		$TEartefacts = $this->getEntityManager()->getRepository('Collection\Entity\TypeElement')->findBy(array('type'=>'artefact'));
-	    		return new ViewModel(array('types' => $TEartefacts, 'form' => $form, 'type' => $type_element));
-
-	    	}
-    	}
-=======
->>>>>>> 5804f49ff0dcf8313c013127a02e7599a06724d5
     	$TEartefacts = $this->getEntityManager()->getRepository('Collection\Entity\TypeElement')->findBy(array('type'=>'artefact'));
-    	return new ViewModel(array('types' => $TEartefacts, 'form' => null, 'type' => 'none'));
+    	return new ViewModel(array('types' => $TEartefacts, 'form' => null));
     }
 
 	public function getFormAjaxAction()
