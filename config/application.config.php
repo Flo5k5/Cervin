c347f365e36d83b343b2fc46442b19c00bd15a97
Corentin <corentin.gaillot@mail.sogeti.com>
2013-05-07 08:46:54 +0200

    conflits

--- config/application.config.php
+++ config/application.config.php
@@ -26,6 +26,8 @@ return array(
             './module',
             './vendor'
             ),
-        'config_glob_paths' => array('config/autoload/{,*.}{global,local}.php')
+        'config_glob_paths' => array(
+            'config/autoload/{,*.}{global,local}.php'
         )
-    );
+    )
+);
