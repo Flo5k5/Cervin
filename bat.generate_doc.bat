c919009835ae34f37931a2aa88e1ae83ee356dfb
Florian <fnourrisse@gmail.com>
2013-05-29 11:18:30 +0200

    Correction des problèmes avec phpDoc2

--- bat.generate_doc.bat
+++ bat.generate_doc.bat
@@ -1,16 +1,27 @@
 @echo off
-set module="%~dp0module"
-set doc="%~dp0doc"
+set module=%~dp0module
+set doc=%~dp0doc\phpDoc
 
 echo.
 echo Code folder : %module%
 echo Destination folder : %doc%
 echo Generating documentation ...
 echo.
-php phpDocumentor.phar -d %module% -t %doc%
+call phpdoc -d "%module%" -t "%doc%"
 echo.
+if exist "%doc%\index.html" goto LAUNCH
+goto EXIT
+
+:LAUNCH
+echo.
+echo Press any key to open report
+pause > nul
+start chrome --allow-file-access-from-files "%doc%\index.html"
+exit
+
 
 :EXIT
 echo.
 echo Press any key to quit
 pause > nul
+exit
