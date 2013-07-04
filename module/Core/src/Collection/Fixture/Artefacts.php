620277a8991633e6c059fe051e6fd9d8feb1614f
Thibaud Avenier <tavenier@FR-69-01-11-047.corp.capgemini.com>
2013-07-04 16:59:44 +0200

    ajout des root view dans module.config

--- module/Core/src/Collection/Fixture/Artefacts.php
+++ module/Core/src/Collection/Fixture/Artefacts.php
@@ -31,6 +31,40 @@ class Artefacts implements FixtureInterface
 		$manager->persist($selectPays);
 
 
+		$liste_des_departements_fr = array(
+"Ain","Aisne","Allier","Alpes-de-Haute-Provence","Hautes-Alpes","Alpes-Maritimes","Ardèche","Ardennes","Ariège","Aube","Aude","Aveyron","Bouches-du-Rhône","Calvados","Cantal","Charente","Charente-Maritime","Cher","Corrèze","Corse-du-Sud","Haute-Corse","Côte-d'Or","Côtes-d'Armor","Creuse","Dordogne","Doubs","Drôme","Eure","Eure-et-Loir","Finistère","Gard","Haute-Garonne","Gers","Gironde","Hérault","Ille-et-Vilaine","Indre","Indre-et-Loire","Isère","Jura","Landes","Loir-et-Cher","Loire","Haute-Loire","Loire-Atlantique","Loiret","Lot","Lot-et-Garonne","Lozère","Maine-et-Loire","Manche","Marne","Haute-Marne","Mayenne","Meurthe-et-Moselle","Meuse","Morbihan","Moselle","Nièvre","Nord","Oise","Orne","Pas-de-Calais","Puy-de-Dôme","Pyrénées-Atlantiques","Hautes-Pyrénées","Pyrénées-Orientales","Bas-Rhin","Haut-Rhin","Rhône","Haute-Saône","Saône-et-Loire","Sarthe","Savoie","Haute-Savoie","Paris","Seine-Maritime","Seine-et-Marne","Yvelines","Deux-Sèvres","Somme","Tarn","Tarn-et-Garonne","Var","Vaucluse","Vendée","Vienne","Haute-Vienne","Vosges","Yonne","Territoire de Belfort","Essonne","Hauts-de-Seine","Seine-Saint-Denis","Val-de-Marne","Val-d'Oise","Guadeloupe","Martinique","Guyane","La Réunion",			);
+
+		$select_departements_fr = new Collection\Entity\Select();
+		$select_departements_fr->__set('description', 'Liste des départements français');
+		$select_departements_fr->__set('label', 'Pays');
+
+		foreach ($liste_des_departements_fr as $key => $dp) {
+			$select_option =  new Collection\Entity\SelectOption($select_departements_fr);
+			$select_option->__set('text',$dp);
+			$manager->persist($select_option);
+
+		}
+		
+		$manager->persist($select_departements_fr);
+
+
+$liste_states = array(
+		"Aquitaine","Auvergne","Bourgogne","Bretagne","Centre","Champagne Ardenne","Corse","DOM/TOM","Franche Comté","Ile de France","Languedoc Roussillon","Limousin","Lorraine","Midi Pyrénées","Nord Pas de Calais","Haute Normandie","Pays de la Loire","Picardie","Poitou Charentes","Provence Alpes Côte d'azur","Rhône Alpes","Alsace","Basse-Normandie");
+
+		$states = new Collection\Entity\Select();
+		$states->__set('description', 'Liste des régions français');
+		$states->__set('label', 'Pays');
+
+		foreach ($liste_states as $key => $dp) {
+			$select_option =  new Collection\Entity\SelectOption($states);
+			$select_option->__set('text',$dp);
+			$manager->persist($select_option);
+
+		}
+		
+		$manager->persist($states);
+
+
 
 
 		/* *********************************** *
