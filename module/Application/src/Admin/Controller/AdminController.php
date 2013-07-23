4f2671021e80d8fb2711793a97920bd528c95244
Florian <florian.nourrisse@mail.sogeti.com>
2013-07-23 17:03:22 +0200

    Ajout d'une modale pour afficher les éléments modifiés dans les logs

--- module/Application/src/Admin/Controller/AdminController.php
+++ module/Application/src/Admin/Controller/AdminController.php
@@ -838,15 +838,17 @@ class AdminController extends AbstractActionController
             }
                 
             foreach ($dataTable->getPaginator() as $log) {
+
             	$datasModified = "";
-            	$revertButton = "";
+            	$revertButton  = "";
+
             	if ($log->getAction() != 'remove') {
-	                //var_dump($log->getData());
+
 	                $revertButton = '<a href="#" 
 	                    data-url="'.$this->url()->fromRoute("admin/revert-object").'"
-	                    data-version="'.$log->getVersion().'"
-	                    data-object="'.$log->getObjectClass().'"
-	                    data-id="'.$log->getObjectId().'"
+	                    data-version="'.$escapeHtml($log->getVersion()).'"
+	                    data-object="'.$escapeHtml($log->getObjectClass()).'"
+	                    data-id="'.$escapeHtml($log->getObjectId()).'"
 	                    class="btn btn-danger revertObject popoverTop"
 	                    data-toggle="popover"
 	                    data-content="Revenir à cette version">
@@ -856,22 +858,30 @@ class AdminController extends AbstractActionController
 	                $popoverDatasModified = null;
 					
 	                if ($log->getData()) {
+
 		                foreach($log->getData() as $name => $data){
 		                    if(is_array($data) || is_object($data)){ $data = json_encode($data); }
-		                    $popoverDatasModified .= ucfirst($name).' : "'.$data.'"<br/>';
+
+                            if(strlen($data) > 30){ $data = $this->truncate( $data, 30, ' ...', true, true ); }
+
+		                    $popoverDatasModified .= $escapeHtml(ucfirst($name))." : '".$escapeHtml($data)."'<br/>";
 		                }
 	                
-	                	$popoverDatasModified = str_replace("\"", "'", $popoverDatasModified);
+	                	//$popoverDatasModified = str_replace("\"", "'", $popoverDatasModified);
 	                }
+
 	                $datasModified = '<a href="#"
-	                                class="Info popoverLeft"
+	                                class="InfoObject popoverLeft"
 	                                data-toggle="popover"
 	                                data-html="true"
 	                                data-content="'.$popoverDatasModified.'"
-	                                data-title="Donnée(s) modifiée(s)">
+	                                data-title="Donnée(s) modifiée(s)"
+                                    data-data="'.$escapeHtml(json_encode($log->getData())).'"
+                                    >
 	                                    <i class="icon-copy"></i>
 	                                </a>';
                 }
+
                 $action = '<div class="pull-left">'.$revertButton.'</div><div class="pull-right">'.$datasModified.'</div>';
 
                 $aaData[] = array(
@@ -1044,6 +1054,109 @@ Bonne journée! ")
     	
     	}
     	return new ViewModel(array('form'=>$form));
+    }
+
+
+    /**
+     * Truncates text.
+     *
+     * Cuts a string to the length of $length and replaces the last characters
+     * with the ending if the text is longer than length.
+     * Crédit : http://www.ycerdan.fr/php/tronquer-un-texte-en-conservant-les-tags-html-en-php/
+     *
+     * @param string  $text String to truncate.
+     * @param integer $length Length of returned string, including ellipsis.
+     * @param mixed $ending If string, will be used as Ending and appended to the trimmed string. Can also be an associative array that can contain the last three params of this method.
+     * @param boolean $exact If false, $text will not be cut mid-word
+     * @param boolean $considerHtml If true, HTML tags would be handled correctly
+     * @return string Trimmed string.
+     */
+    private function truncate($text, $length = 100, $ending = '...', $exact = true, $considerHtml = false) {
+        if (is_array($ending)) {
+            extract($ending);
+        }
+        if ($considerHtml) {
+            if (mb_strlen(preg_replace('/<.*?>/', '', $text)) <= $length) {
+                return $text;
+            }
+            $totalLength = mb_strlen($ending);
+            $openTags = array();
+            $truncate = '';
+            preg_match_all('/(<\/?([\w+]+)[^>]*>)?([^<>]*)/', $text, $tags, PREG_SET_ORDER);
+            foreach ($tags as $tag) {
+                if (!preg_match('/img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param/s', $tag[2])) {
+                    if (preg_match('/<[\w]+[^>]*>/s', $tag[0])) {
+                        array_unshift($openTags, $tag[2]);
+                    } else if (preg_match('/<\/([\w]+)[^>]*>/s', $tag[0], $closeTag)) {
+                        $pos = array_search($closeTag[1], $openTags);
+                        if ($pos !== false) {
+                            array_splice($openTags, $pos, 1);
+                        }
+                    }
+                }
+                $truncate .= $tag[1];
+    
+                $contentLength = mb_strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', ' ', $tag[3]));
+                if ($contentLength + $totalLength > $length) {
+                    $left = $length - $totalLength;
+                    $entitiesLength = 0;
+                    if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', $tag[3], $entities, PREG_OFFSET_CAPTURE)) {
+                        foreach ($entities[0] as $entity) {
+                            if ($entity[1] + 1 - $entitiesLength <= $left) {
+                                $left--;
+                                $entitiesLength += mb_strlen($entity[0]);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+    
+                    $truncate .= mb_substr($tag[3], 0 , $left + $entitiesLength);
+                    break;
+                } else {
+                    $truncate .= $tag[3];
+                    $totalLength += $contentLength;
+                }
+                if ($totalLength >= $length) {
+                    break;
+                }
+            }
+    
+        } else {
+            if (mb_strlen($text) <= $length) {
+                return $text;
+            } else {
+                $truncate = mb_substr($text, 0, $length - strlen($ending));
+            }
+        }
+        
+        if (!$exact) {
+            $spacepos = mb_strrpos($truncate, ' ');
+            if (isset($spacepos)) {
+                if ($considerHtml) {
+                    $bits = mb_substr($truncate, $spacepos);
+                    preg_match_all('/<\/([a-z]+)>/', $bits, $droppedTags, PREG_SET_ORDER);
+                    if (!empty($droppedTags)) {
+                        foreach ($droppedTags as $closingTag) {
+                            if (!in_array($closingTag[1], $openTags)) {
+                                array_unshift($openTags, $closingTag[1]);
+                            }
+                        }
+                    }
+                }
+                $truncate = mb_substr($truncate, 0, $spacepos);
+            }
+        }
+    
+        $truncate .= $ending;
+    
+        if ($considerHtml) {
+            foreach ($openTags as $tag) {
+                $truncate .= '</'.$tag.'>';
+            }
+        }
+    
+        return $truncate;
     }
 
 }
